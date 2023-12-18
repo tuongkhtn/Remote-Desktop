5ac238be5cb8069238da2d08b5021ab2731bf373
tuong <110910077+tuongkhtn@users.noreply.github.com>
2023-12-19 00:02:47 +0700

    Add files via upload

--- Client/main.cpp
+++ Client/main.cpp
@@ -6,22 +6,33 @@ Mouse mouseState;
 
 int main() {
 
-	sf::RenderWindow window(sf::VideoMode(SCREEN_WIDTH, SCREEN_HEIGHT), "Network Programming");
 
-	std::vector<std::pair<char*, int>> connected_params;
-	connected_params.push_back({ (char*)"172.20.16.161", 8080 });
-	connected_params.push_back({ (char*)"172.20.34.42", 8080 });
-	connected_params.push_back({ (char*)"172.20.10.195", 8080 });
+	// Set ip and port
+	int numOfServer;
+	std::cout << "Enter the server number: ";
+	std::cin >> numOfServer;
 
-	Slide1 slide1;
-	Client client;
+	if (numOfServer > 0) {
+		std::vector<std::pair<char*, int>> connected_params(numOfServer);
+		for (int i = 0; i < connected_params.size(); i++) {
+			char* ip = new char[20];
+			int port = 8080;
+			std::cout << "Enter IP Adress: ";
+			std::cin >> ip;
+			std::cin.ignore();
+			connected_params[i] = { ip, port };
+		}
+		sf::RenderWindow window(sf::VideoMode(SCREEN_WIDTH, SCREEN_HEIGHT), "Network Programming");
+		Slide1 slide1;
+		Client client;
 
-	client.setConnectPars(connected_params);
+		client.setConnectPars(connected_params);
 
-	slide1.run(window, client);
+		slide1.run(window, client);
 
-	for (int i = 0; i < connected_params.size(); i++) {
-		delete connected_params[i].first;
+		for (int i = 0; i < connected_params.size(); i++) {
+			delete connected_params[i].first;
+		}
 	}
 
 	return 0;
